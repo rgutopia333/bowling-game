2f6279a2a5e56a89f4eae35a06202c35e3c2d906
rgutopia333 <rgfitnsuccess@proton.me>
2022-09-19 10:10:38 -0400

    Bowling score calculation logic completed

--- src/app/score-game/score-game.component.ts
+++ src/app/score-game/score-game.component.ts
@@ -1,79 +1,128 @@
 import { Component, OnInit } from '@angular/core';
+import { Frames, frames, frames2 } from '../../bowlingGame';
 
 @Component({
   selector: 'app-score-game',
   templateUrl: './score-game.component.html',
-  styleUrls: ['./score-game.component.css']
+  styleUrls: ['./score-game.component.css'],
 })
 export class ScoreGameComponent implements OnInit {
-  headers = ["Frame", "Roll 1", "Roll 2", "Roll 3"];
+  headers = ['Frame', 'Roll1', 'Roll2', 'Roll3'];
+  score = 0;
 
-  frames: any= [
+  columns: any = [
     {
-      "Frame" : "1",
-      "Roll 1" : "4",
-      "Roll 2" : "3",
-      "Roll 3" : ""
+      Frame: '1',
+      Roll1: '4',
+      Roll2: '3',
+      Roll3: '',
     },
     {
-      "Frame" : "2",
-      "Roll 1" : "7",
-      "Roll 2" : "3",
-      "Roll 3" : ""
+      Frame: '2',
+      Roll1: '7',
+      Roll2: '3',
+      Roll3: '',
     },
     {
-      "Frame" : "3",
-      "Roll 1" : "5",
-      "Roll 2" : "2",
-      "Roll 3" : ""
+      Frame: '3',
+      Roll1: '5',
+      Roll2: '2',
+      Roll3: '',
     },
     {
-      "Frame" : "4",
-      "Roll 1" : "8",
-      "Roll 2" : "1",
-      "Roll 3" : ""
+      Frame: '4',
+      Roll1: '8',
+      Roll2: '1',
+      Roll3: '',
     },
     {
-      "Frame" : "5",
-      "Roll 1" : "4",
-      "Roll 2" : "6",
-      "Roll 3" : ""
+      Frame: '5',
+      Roll1: '4',
+      Roll2: '6',
+      Roll3: '',
     },
     {
-      "Frame" : "6",
-      "Roll 1" : "2",
-      "Roll 2" : "4",
-      "Roll 3" : ""
+      Frame: '6',
+      Roll1: '2',
+      Roll2: '4',
+      Roll3: '',
     },
     {
-      "Frame" : "7",
-      "Roll 1" : "8",
-      "Roll 2" : "0",
-      "Roll 3" : ""
+      Frame: '7',
+      Roll1: '8',
+      Roll2: '0',
+      Roll3: '',
     },
     {
-      "Frame" : "8",
-      "Roll 1" : "8",
-      "Roll 2" : "0",
-      "Roll 3" : ""
+      Frame: '8',
+      Roll1: '8',
+      Roll2: '0',
+      Roll3: '',
     },
     {
-      "Frame" : "9",
-      "Roll 1" : "8",
-      "Roll 2" : "2",
-      "Roll 3" : ""
+      Frame: '9',
+      Roll1: '8',
+      Roll2: '2',
+      Roll3: '',
     },
     {
-      "Frame" : "10",
-      "Roll 1" : "10",
-      "Roll 2" : "1",
-      "Roll 3" : "7"
+      Frame: '10',
+      Roll1: '10',
+      Roll2: '1',
+      Roll3: '7',
+    },
+  ];
+
+  constructor() {}
+  ngOnInit() {
+    if (frames != null) {
+      frames.forEach((currentFrame) => {
+        var nextFrame = frames[currentFrame.Frame];
+        var followingNextFrame = frames[currentFrame.Frame + 1];
+        this.addCurrentFrameRolls(currentFrame);
+
+        if (nextFrame != null && currentFrame.Roll1 == 10) {
+          this.score = this.addFollowingFrameRolls(nextFrame);
+
+          this.score =
+            followingNextFrame != null && nextFrame.Roll1 == 10
+              ? (this.score = this.score + followingNextFrame.Roll1)
+              : (this.score = this.score + currentFrame.Roll2);
+        }
+
+        if (
+          nextFrame != null &&
+          currentFrame.Roll1 < 10 &&
+          currentFrame.Roll1 + currentFrame.Roll2 == 10
+        ) {
+          this.addFollowingFrameFirstRoll(nextFrame);
+        }
+      });
     }
-  ]
-
-  constructor() { }
-
-  ngOnInit(): void {
   }
 
+  private addCurrentFrameRolls(currentFrame: Frames) {
+    this.score = this.score + currentFrame.Roll1 + currentFrame.Roll2;
+
+    // this condition ensures that either a strike or spare is bowled in the 10th frame before accounting for the third roll
+    if (
+      currentFrame.Frame == 10 &&
+      (currentFrame.Roll1 == 10 ||
+        currentFrame.Roll1 + currentFrame.Roll2 == 10)
+    ) {
+      this.score = this.score + currentFrame.Roll3;
+    }
+  }
+
+  private addFollowingFrameRolls(nextFrame: Frames) {
+    if (nextFrame != null) {
+      this.score = this.score + nextFrame.Roll1 + nextFrame.Roll2;
+    }
+
+    return this.score;
+  }
+
+  private addFollowingFrameFirstRoll(nextFrame: Frames) {
+    this.score = this.score + nextFrame.Roll1;
+  }
 }
---
+++ src/bowlingGame.ts
@@ -0,0 +1,132 @@
+export interface Frames {
+    Frame: number;
+    Roll1: number;
+    Roll2: number;
+    Roll3: number;
+  }
+
+  export const frames : Array<Frames> = [
+    {
+      Frame : 1,
+      Roll1 : 4,
+      Roll2 : 3,
+      Roll3 : 0
+    },
+    {
+      Frame : 2,
+      Roll1 : 7,
+      Roll2 : 3,
+      Roll3 : 0
+    },
+    {
+      Frame : 3,
+      Roll1 : 5,
+      Roll2 : 2,
+      Roll3 : 0
+    },
+    {
+      Frame : 4,
+      Roll1 : 8,
+      Roll2 : 1,
+      Roll3 : 0
+    },
+    {
+        Frame : 5,
+        Roll1 : 4,
+        Roll2 : 6,
+        Roll3 : 0
+    },
+    {
+        Frame : 6,
+        Roll1 : 2,
+        Roll2 : 4,
+        Roll3 : 0
+    },
+    {
+        Frame : 7,
+        Roll1 : 8,
+        Roll2 : 0,
+        Roll3 : 0
+    },
+    {
+        Frame : 8,
+        Roll1 : 8,
+        Roll2 : 0,
+        Roll3 : 0
+    },
+    {
+        Frame : 9,
+        Roll1 : 8,
+        Roll2 : 2,
+        Roll3 : 0
+    },
+    {
+        Frame : 10,
+        Roll1 : 10,
+        Roll2 : 1,
+        Roll3 : 7
+    }
+  ]
+
+  export const frames2 : Array<Frames> = [
+    {
+      Frame : 1,
+      Roll1 : 10,
+      Roll2 : 0,
+      Roll3 : 0
+    },
+    {
+      Frame : 2,
+      Roll1 : 10,
+      Roll2 : 0,
+      Roll3 : 0
+    },
+    {
+      Frame : 3,
+      Roll1 : 10,
+      Roll2 : 0,
+      Roll3 : 0
+    },
+    {
+      Frame : 4,
+      Roll1 : 7,
+      Roll2 : 2,
+      Roll3 : 0
+    },
+    {
+        Frame : 5,
+        Roll1 : 10,
+        Roll2 : 0,
+        Roll3 : 0
+    },
+    {
+        Frame : 6,
+        Roll1 : 10,
+        Roll2 : 0,
+        Roll3 : 0
+    },
+    {
+        Frame : 7,
+        Roll1 : 6,
+        Roll2 : 4,
+        Roll3 : 0
+    },
+    {
+        Frame : 8,
+        Roll1 : 9,
+        Roll2 : 1,
+        Roll3 : 0
+    },
+    {
+        Frame : 9,
+        Roll1 : 2,
+        Roll2 : 2,
+        Roll3 : 0
+    },
+    {
+        Frame : 10,
+        Roll1 : 10,
+        Roll2 : 10,
+        Roll3 : 10
+    }
+  ]
